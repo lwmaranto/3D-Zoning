35d86b6609969ce4ce78247f72b875914b2ce361
lwmaranto <laurawmaranto@gmail.com>
2021-02-06 20:20:16 -0700

    code clean up

--- server/index.js
+++ server/index.js
@@ -1,38 +1,35 @@
-const express = require('express')
-const path = require('path')
-const volleyball = require('volleyball')
+const express = require("express");
+const path = require("path");
+const volleyball = require("volleyball");
 
-
-const app = express()
+const app = express();
 
 // logging middleware
 // Only use logging middleware when not running tests
-const debug = process.env.NODE_ENV === 'test'
-app.use(volleyball.custom({ debug }))
+const debug = process.env.NODE_ENV === "test";
+app.use(volleyball.custom({ debug }));
 
 // body parsing middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //body parsing middleware
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // static middleware
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use('/api', require('./api'))
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-}) // Send index.html for any other requests
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+}); // Send index.html for any other requests
 
 // error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err);  
-  console.error(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error')
-})
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error");
+});
 
-module.exports = app
+module.exports = app;
